20c683ec84b10e49f279b07b0b7f507cfe6f6810
kishanzaveri312-max <kishanzaveri312@gmail.com>
2026-05-21 07:40:17 +0000

    Fix script

---
+++ script.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "========================================="
+echo "Welcome to DKcloud!"
+echo "Aapki custom script perfectly kaam kar rahi hai."
+echo "========================================="
